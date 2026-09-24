a76abe4ef6bd9d988005023692d52726ef34c0f1
Duck <duck@duck-things.org>
2026-09-24 19:22:33 -0400

    taco deep cosmos

---
+++ main/src/kotlin/sleepy/alsina/DeepCosmos/Main.kt
@@ -0,0 +1,13 @@
+package sleepy.asina.DeepCosmos
+
+import duck.duckthings.DeepCosmos
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
+import org.bukkit.event.Listener
+import org.bukkit.generator.BiomeProvider
+import org.bukkit.generator.ChunkGenerator
+import org.bukkit.plugin.java.JavaPlugin
+
+fun main() {
+  println("Hello, Taco's Deep Cosmos is working.")
+}
